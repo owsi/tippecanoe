ea150a3d00ea5eb798ccf2e97101cd76d80b1407
Stephen E Ingram <singram@openwhere.com>
2017-10-19 13:36:52 -0400

    Add EPSG:3395 option to projections
    Write Ellipsoid Mercator instead of Spherical Mercator,
    otherwise known as Web Mercator

--- projection.cpp
+++ projection.cpp
@@ -7,6 +7,7 @@
 struct projection projections[] = {
 	{"EPSG:4326", lonlat2tile, tile2lonlat, "urn:ogc:def:crs:OGC:1.3:CRS84"},
 	{"EPSG:3857", epsg3857totile, tiletoepsg3857, "urn:ogc:def:crs:EPSG::3857"},
+	{"EPSG:3395", lonlat2epsg3395tile, epsg3395tiletolonlat, "urn:ogc:def:crs:EPSG::3395"},
 	{NULL, NULL},
 };
 
@@ -47,6 +48,99 @@ void tile2lonlat(long long x, long long y, int zoom, double *lon, double *lat) {
 	*lon = 360.0 * x / n - 180.0;
 	*lat = atan(sinh(M_PI * (1 - 2.0 * y / n))) * 180.0 / M_PI;
 }
+
+void lonlat2epsg3395tile(double lon, double lat, int zoom, long long *x, long long *y) {
+	// Must limit latitude somewhere to prevent overflow.
+	// 89.9 degrees latitude is 0.621 worlds beyond the edge of the flat earth,
+	// hopefully far enough out that there are few expectations about the shape.
+	if (lat < -89.9) {
+		lat = -89.9;
+	}
+	if (lat > 89.9) {
+		lat = 89.9;
+	}
+
+	if (lon < -360) {
+		lon = -360;
+	}
+	if (lon > 360) {
+		lon = 360;
+	}
+	double a = 6378137.0;
+	double e = 0.081819190842622;
+    unsigned long long n = 1LL << zoom;
+
+	double lat_rad = lat*M_PI/180.0;
+	double lon_rad = lon*M_PI/180.0;
+	double dx = a * lon_rad;
+	double d1 = tan(M_PI/4 + lat_rad/2);
+	double sinLat = e * sin(lat_rad);
+	double dt = 1.0 - sinLat;
+	double dt1 = 1.0 + sinLat;
+	double dy = a * log(d1 * pow(dt/dt1, e/2));
+
+    double circumference = 2.0 * M_PI * 6378137.0;
+	double res = circumference / 256.0;
+	res = res / exp2((double)zoom);
+    double originShift = circumference / 2.0;
+	double px = (dx + originShift) / res;
+	double py = (dy + originShift) / res;
+    long long llx = ceil(px/256.0) - 1;
+    long long lly = ceil(py/256.0) - 1;
+
+    // Store as Google tile (flip y)
+	*x = llx;
+	*y = (n - 1) - lly;
+}
+
+void epsg3395tiletolonlat(long long x, long long y, int zoom, double *lon, double *lat) {
+	unsigned long long n = 1LL << zoom;
+	double a = 6378137.0;
+	double e = 0.081819190842622;
+    double circumference = 2.0 * M_PI * 6378137.0;
+    // Convert back to TMS from Google tile (flip y)
+    long long ny = (n - 1) - y;
+
+	double res = circumference / 256.0;
+	res = res / n;
+
+	// First unmap back to pixels
+	long long px = (x + 1) * 256;
+	long long py = (ny + 1) * 256;
+
+	// Now back to meters
+    double originShift = circumference / 2.0;
+ 	double dx = px/res - originShift;
+ 	double dy = py/res - originShift;
+
+ 	// Now iterate to arrive at lat (radians)
+ 	double t = exp(-dy/a);
+ 	double tlat = M_PI/2 - atan(t);
+ 	double EPSILON = 0.00000001;
+ 	bool changing = true;
+ 	double term = e * sin(tlat);
+ 	double prev = M_PI/2 - (2 * atan(t * pow((1 - term)/(1 + term), e/2)));
+ 	while (changing) {
+ 	    term = e * sin(tlat);
+ 	    tlat = M_PI/2 - (2 * atan(t * pow((1 - term)/(1 + term), e/2)));
+
+        double diff = tlat - prev;
+        if (diff < 0.0) {
+            diff = diff * -1.0;
+        }
+ 	    if (diff <= EPSILON ) {
+ 	        changing = false;
+ 	     } else {
+ 	        prev = tlat;
+ 	     }
+
+ 	}
+
+ 	// Back to decimal degrees
+    *lat = tlat * (180.0/M_PI);
+ 	*lon = (dx/a) * (180.0/M_PI);
+}
+
 
 void epsg3857totile(double ix, double iy, int zoom, long long *x, long long *y) {
 	*x = ix * (1LL << 31) / 6378137.0 / M_PI + (1LL << 31);
--- projection.hpp
+++ projection.hpp
@@ -5,6 +5,8 @@ void lonlat2tile(double lon, double lat, int zoom, long long *x, long long *y);
 void epsg3857totile(double ix, double iy, int zoom, long long *x, long long *y);
 void tile2lonlat(long long x, long long y, int zoom, double *lon, double *lat);
 void tiletoepsg3857(long long x, long long y, int zoom, double *ox, double *oy);
+void lonlat2epsg3395tile(double lon, double lat, int zoom, long long *x, long long *y);
+void epsg3395tiletolonlat(long long x, long long y, int zoom, double *lon, double *lat);
 unsigned long long encode(unsigned int wx, unsigned int wy);
 void decode(unsigned long long index, unsigned *wx, unsigned *wy);
 void set_projection_or_exit(const char *optarg);
